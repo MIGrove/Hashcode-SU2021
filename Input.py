7d8bacb7f28311f40ffefdf3a8a180215e5f3ce5
Ridalise <ridalise@gmail.com>
2021-02-25 21:18:38 +0200

    Input

--- Input.py
+++ Input.py
@@ -1,13 +1,31 @@
-inFiles = ["a.txt", "b.txt", "c.txt", "d.txt", "e.txt", "f.txt"]
+from Street import Street
+from Car import Car
 
-for j in range(6):
+def input(inFiles):
+    for j in range(6):
 
-    filename = inFiles[j]
-    infile = open(filename, 'r')
+        filename = inFiles[j]
+        infile = open(filename, 'r')
 
-    numbers = [int(m) for m in infile.readline().split()]
-    simDuration = numbers[0]
-    nIntersections = numbers[1]
-    nStreets = numbers[2]
-    nCars = numbers[3]
-    points = numbers[4]
+        numbers = [int(m) for m in infile.readline().split()]
+        simDuration = numbers[0]
+        nIntersections = numbers[1]
+        nStreets = numbers[2]
+        nCars = numbers[3]
+        points = numbers[4]
+
+    streetList = []
+    for _ in range(nStreets):
+        streetParams = [int(m) for m in infile.readline().split()]
+        streetList.append( Street(streetParams[0],streetParams[1],streetParams[2],streetParams[3]) ) 
+
+    carList = []
+    for _ in range(nCars):     
+        carParams = [int(m) for m in infile.readline().split()]
+        streetNames = []
+        for j in range(carParams[0]):
+            streetNames.append(carParams[j+1])
+
+        carList.append( Car(carParams[0], streetNames) )
+
+    return(simDuration, nIntersections, nStreets, nCars, points, streetList, carList)
---
+++ Main.py
@@ -0,0 +1,4 @@
+import math
+
+inFiles = ["a_example.txt", "b_read_on.txt", "c_incunabula.txt", "d_tough_choices.txt", "e_so_many_books.txt", "f_libraries_of_the_world.txt"]
+outFiles = ["a_out.txt", "b_out.txt","c_out.txt","d_out.txt","e_out.txt","f_out.txt"]
